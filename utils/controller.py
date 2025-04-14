218f4addfe17ae3defdc41772cceca5d40b2ca3a
Julcik25 <juliaz@vip.onet.pl>
2025-04-14 09:44:08 +0200

    wip: new menu option: update user

--- utils/controller.py
+++ utils/controller.py
@@ -7,3 +7,18 @@ def add_user(users_data:list[dict]) -> None:
     tmp_location:str= input('podaj miejsce: ')
     tmp_posts:int = int(input('podaj liczbę postów: '))
     users_data.append({'name': tmp_name, 'location': tmp_location, 'posts': tmp_posts, })
+
+def remove_user(users_data: list[dict]) -> None:
+    user_name = input('Podaj imię użytkownika do usunięcia: ')
+    for user in users_data:
+        if user['name'] == user_name:
+            users_data.remove(user)
+
+def update_user(users_data: list[dict]) -> None:
+    user_name = input('Podaj imię użytkownika do zmodyfikowania: ')
+    for user in users_data:
+        if user['name'] == user_name:
+
+            user['name']=input('Podaj nowe imię: ')
+            user['location']=input('Podaj nową lokalizację: ')
+            user['posts']=input('Podaj nową liczbę postów: ')
