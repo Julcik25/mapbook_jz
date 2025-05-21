33045a6bac8fa49994b5b68562588da06558d86f
Julcik25 <juliaz@vip.onet.pl>
2025-05-21 09:36:50 +0200

    wip: fronted z interaktywną mapą

--- utils/model.py
+++
@@ -1,5 +0,0 @@
-users: list[dict] = [
-    {'name': 'Julia', 'location': 'Płońsk', 'posts': 500, },
-    {'name': 'Emilia', 'location': 'Lublin', 'posts': 400, },
-    {'name': 'Martyna', 'location': 'Ząbki', 'posts': 200, }
-]
